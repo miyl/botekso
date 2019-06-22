0e81b8982942b3803a17f0d835c1b73af763ac8e
Marcus Miyalys <resolutions@riseup.net>
2019-06-22 18:42:37 +0200

    Added application.properties for the test server, and made curl output silent/quiet

--- test-client.sh
+++ test-client.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env sh
 
-curl --data 'key=vipGo8LbUdsVPM2vBxBXuLEh46DV78Ya5CSD9vcgEy7pwXuhWYAgB3ANnrWAz7wxEHeSfX9CfsEtzNvGPL4HCvpKXi7KNMSa4ZJd' localhost:8080/api/execute/1
+curl -s --data 'key=vipGo8LbUdsVPM2vBxBXuLEh46DV78Ya5CSD9vcgEy7pwXuhWYAgB3ANnrWAz7wxEHeSfX9CfsEtzNvGPL4HCvpKXi7KNMSa4ZJd' localhost:8080/api/execute/1
